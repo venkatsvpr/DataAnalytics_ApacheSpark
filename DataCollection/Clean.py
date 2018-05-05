ba248092e9d29c474c68c1bc81a2f4adda6fda05
Pratibha Barsale <pbarsale@buffalo.edu>
2018-05-05 13:18:53 -0400

    Data Collected and cleaned

---
+++ DataCollection/Clean.py
@@ -0,0 +1,56 @@
+import urllib2
+import json
+from os import listdir
+from bs4 import BeautifulSoup
+import string
+import sys
+
+def getHTMLData(link):
+
+    try:
+        f_data = urllib2.urlopen(html)
+        return f_data
+    except:
+        return None
+
+sections = ['Business Day','Technology']
+section_name=''
+count = 1
+
+file_names = [f for f in listdir("Data_Collected/JSONs")]
+
+for name in file_names:
+    data = open('Data_Collected/JSONs/' + name).read()
+    frame_data = json.loads(data)
+    
+    for item in range(len(frame_data['response']['docs'])):
+        section = frame_data['response']['docs'][item]['section_name']
+
+        if(section in sections):
+            
+            html = frame_data['response']['docs'][item]['web_url']
+            f = getHTMLData(html)
+            
+            if(f!=None):
+
+                if(section=='Business Day'):
+                    section_name = 'Business'
+                else:
+                    section_name = 'Tech'
+
+                ff = open("Data_Collected/"+section_name+"/"+str(count)+".txt","w+")
+                count=count+1
+                html_doc = f.read()
+                soup = BeautifulSoup(html_doc, 'html.parser')
+        
+                for article in soup.find_all('article'):
+                    for para in article.find_all('p'):
+                        line = para.get_text().encode('utf-8')
+                        ff.write(line)
+                ff.close()
+
+
+
+
+
+
